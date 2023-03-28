4ad393ad360ef2e362a1b7293f9f138650960df6
Muhammad Talha Dar <muhammadtalhadar@gmail.com>
2023-03-28 15:03:40 +0500

    integrated balances-erc20 precompile into peaq-dev runtime

--- runtime/peaq-dev/src/precompiles.rs
+++ runtime/peaq-dev/src/precompiles.rs
@@ -1,9 +1,35 @@
+use pallet_evm_precompile_balances_erc20::{Erc20BalancesPrecompile, Erc20Metadata};
 use pallet_evm_precompile_blake2::Blake2F;
 use pallet_evm_precompile_bn128::{Bn128Add, Bn128Mul, Bn128Pairing};
 use pallet_evm_precompile_modexp::Modexp;
 use pallet_evm_precompile_sha3fips::Sha3FIPS256;
 use pallet_evm_precompile_simple::{ECRecover, ECRecoverPublicKey, Identity, Ripemd160, Sha256};
 use precompile_utils::precompile_set::*;
+pub struct NativeErc20Metadata;
+
+/// ERC20 metadata for the native token.
+impl Erc20Metadata for NativeErc20Metadata {
+	/// Returns the name of the token.
+	fn name() -> &'static str {
+		"PEAQ token"
+	}
+
+	/// Returns the symbol of the token.
+	fn symbol() -> &'static str {
+		"PEAQ"
+	}
+
+	/// Returns the decimals places of the token.
+	fn decimals() -> u8 {
+		18
+	}
+
+	/// Must return `true` only if it represents the main native currency of
+	/// the network. It must be the currency used in `pallet_evm`.
+	fn is_native_currency() -> bool {
+		true
+	}
+}
 
 /// The following distribution has been decided for the precompiles
 /// 0-1023: Ethereum Mainnet Precompiles
@@ -30,6 +56,8 @@ pub type PeaqPrecompiles<R> = PrecompileSetBuilder<
 				PrecompileAt<AddressU64<1024>, Sha3FIPS256>,
 				// PrecompileAt<AddressU64<1025>, Dispatch<R>>,
 				PrecompileAt<AddressU64<1026>, ECRecoverPublicKey>,
+				// Moonbeam Specific precompiles
+				PrecompileAt<AddressU64<2050>, Erc20BalancesPrecompile<R, NativeErc20Metadata>>,
 			),
 		>,
 	),
