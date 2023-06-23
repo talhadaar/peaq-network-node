b3f1438cc8860ec971a3bded03f3ccb8e0ba9af4
Muhammad Talha Dar <muhammadtalhadar@gmail.com>
2023-06-23 15:31:28 +0500

    WIP - NOT BULDING multicurrency precompile building, node not building

--- precompiles/multicurrency/src/lib.rs
+++ precompiles/multicurrency/src/lib.rs
@@ -16,6 +16,8 @@
 // You should have received a copy of the GNU General Public License
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
 
+#![cfg_attr(not(feature = "std"), no_std)]
+
 // primitives and utils imports
 
 use peaq_primitives_xcm::{currency::CurrencyId, evm::Erc20InfoMappingT, Balance};
