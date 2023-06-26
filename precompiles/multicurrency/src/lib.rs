3fa79bf3663dd9a5ce78926f4e148333d941a08e
Muhammad Talha Dar <muhammadtalhadar@gmail.com>
2023-06-26 15:49:09 +0500

    Implemented multicurrency precompile

---
+++ precompiles/multicurrency/src/lib.rs
@@ -0,0 +1,158 @@
+// This file is part of Acala.
+
+// Copyright (C) 2020-2023 Acala Foundation.
+// SPDX-License-Identifier: GPL-3.0-or-later WITH Classpath-exception-2.0
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+#![cfg_attr(not(feature = "std"), no_std)]
+
+// primitives and utils imports
+
+use peaq_primitives_xcm::{currency::CurrencyId, evm::Erc20InfoMappingT, Balance};
+use precompile_utils::prelude::*;
+use sp_core::{H160, H256, U256};
+use sp_std::{convert::TryInto, marker::PhantomData};
+
+use fp_evm::PrecompileHandle;
+
+// frame imports
+use pallet_evm::AddressMapping;
+
+// orml imports
+use orml_traits::MultiCurrency as MultiCurrencyT;
+
+/// Solidity selector of the Transfer log, which is the Keccak of the Log signature.
+pub const SELECTOR_LOG_TRANSFER: [u8; 32] = keccak256!("transfer(address,address,uint256)");
+
+// /// The `MultiCurrency` impl precompile.
+// ///
+// ///
+// /// `input` data starts with `action` and `currency_id`.
+// ///
+// /// Actions:
+// /// - Query total issuance.
+// /// - Query balance. Rest `input` bytes: `account_id`.
+// /// - Transfer. Rest `input` bytes: `from`, `to`, `amount`.
+pub struct MultiCurrencyPrecompile<Runtime, Metadata: Erc20InfoMappingT>(
+	PhantomData<(Runtime, Metadata)>,
+);
+
+#[precompile_utils::precompile]
+impl<Runtime, Metadata> MultiCurrencyPrecompile<Runtime, Metadata>
+where
+	Metadata: Erc20InfoMappingT,
+	Runtime: pallet_evm::Config + orml_currencies::Config,
+	orml_currencies::Pallet<Runtime>:
+		MultiCurrencyT<Runtime::AccountId, CurrencyId = CurrencyId, Balance = Balance>,
+	H256: From<<Runtime as frame_system::pallet::Config>::AccountId>,
+{
+	#[precompile::public("name()")]
+	#[precompile::view]
+	fn name(handle: &mut impl PrecompileHandle) -> EvmResult<UnboundedBytes> {
+		handle.record_cost(RuntimeHelper::<Runtime>::db_read_gas_cost())?;
+		let currency_id = Metadata::decode_evm_address(handle.context().caller).unwrap();
+
+		Ok(Metadata::name(currency_id).unwrap().into())
+	}
+
+	#[precompile::public("symbol()")]
+	#[precompile::view]
+	fn symbol(handle: &mut impl PrecompileHandle) -> EvmResult<UnboundedBytes> {
+		handle.record_cost(RuntimeHelper::<Runtime>::db_read_gas_cost())?;
+		let currency_id = Metadata::decode_evm_address(handle.context().caller).unwrap();
+
+		Ok(Metadata::symbol(currency_id).unwrap().into())
+	}
+
+	#[precompile::public("decimals()")]
+	#[precompile::view]
+	fn decimals(handle: &mut impl PrecompileHandle) -> EvmResult<u8> {
+		handle.record_cost(RuntimeHelper::<Runtime>::db_read_gas_cost())?;
+		let currency_id = Metadata::decode_evm_address(handle.context().caller).unwrap();
+
+		Ok(Metadata::decimals(currency_id).unwrap())
+	}
+
+	#[precompile::public("total_issuance()")]
+	#[precompile::view]
+	fn total_issuance(handle: &mut impl PrecompileHandle) -> EvmResult<Balance> {
+		handle.record_cost(RuntimeHelper::<Runtime>::db_read_gas_cost())?;
+		let currency_id = Metadata::decode_evm_address(handle.context().caller).unwrap();
+
+		let total_issuance = <orml_currencies::Pallet<Runtime> as MultiCurrencyT<
+			Runtime::AccountId,
+		>>::total_issuance(currency_id);
+
+		Ok(total_issuance)
+	}
+
+	#[precompile::public("balanceOf(address)")]
+	#[precompile::view]
+	fn balance(handle: &mut impl PrecompileHandle, who: Address) -> EvmResult<Balance> {
+		handle.record_cost(RuntimeHelper::<Runtime>::db_read_gas_cost())?;
+		let who: H160 = who.into();
+		let who: Runtime::AccountId = Runtime::AddressMapping::into_account_id(who);
+
+		let currency_id = Metadata::decode_evm_address(handle.context().caller).unwrap();
+
+		// TODO what if currency in question is native currency
+		let balance =
+			<orml_currencies::Pallet<Runtime> as MultiCurrencyT<Runtime::AccountId>>::total_balance(
+				currency_id,
+				&who,
+			);
+
+		Ok(balance)
+	}
+
+	#[precompile::public("transfer(address, address, uint256)")]
+	fn transfer(
+		handle: &mut impl PrecompileHandle,
+		from: Address,
+		to: Address,
+		amount: U256,
+	) -> EvmResult<bool> {
+		handle.record_cost(RuntimeHelper::<Runtime>::db_read_gas_cost())?;
+		let from: H160 = from.into();
+		let from: Runtime::AccountId = Runtime::AddressMapping::into_account_id(from);
+
+		let to: H160 = to.into();
+		let to: Runtime::AccountId = Runtime::AddressMapping::into_account_id(to);
+		let amount: Balance = amount.try_into().unwrap();
+
+		let currency_id = Metadata::decode_evm_address(handle.context().caller).unwrap();
+
+		// TODO build call with origin using RuntimeHelper?
+		<orml_currencies::Pallet<Runtime> as MultiCurrencyT<Runtime::AccountId>>::transfer(
+			currency_id,
+			&from,
+			&to,
+			amount,
+		)
+		.unwrap();
+
+		log4(
+			handle.context().address,
+			SELECTOR_LOG_TRANSFER,
+			handle.context().caller,
+			to,
+			from,
+			EvmDataWriter::new().write(amount).build(),
+		)
+		.record(handle)?;
+
+		Ok(true)
+	}
+}
--- primitives/xcm/src/evm.rs
+++ primitives/xcm/src/evm.rs
@@ -1,2 +1,32 @@
+use crate::currency::CurrencyId;
+
+extern crate alloc;
+use alloc::vec::Vec;
+
 /// Evm Address.
 pub type EvmAddress = sp_core::H160;
+
+/// A mapping between u32 and Erc20 address.
+/// provide a way to encode/decode for CurrencyId;
+pub trait Erc20InfoMappingT {
+	/// Returns the name associated with a given CurrencyId.
+	/// If CurrencyId is CurrencyId::DexShare and contain DexShare::Erc20,
+	/// the EvmAddress must have been mapped.
+	fn name(currency_id: CurrencyId) -> Option<Vec<u8>>;
+	/// Returns the symbol associated with a given CurrencyId.
+	/// If CurrencyId is CurrencyId::DexShare and contain DexShare::Erc20,
+	/// the EvmAddress must have been mapped.
+	fn symbol(currency_id: CurrencyId) -> Option<Vec<u8>>;
+	/// Returns the decimals associated with a given CurrencyId.
+	/// If CurrencyId is CurrencyId::DexShare and contain DexShare::Erc20,
+	/// the EvmAddress must have been mapped.
+	fn decimals(currency_id: CurrencyId) -> Option<u8>;
+	/// Encode the CurrencyId to EvmAddress.
+	/// If is CurrencyId::DexShare and contain DexShare::Erc20,
+	/// will use the u32 to get the DexShare::Erc20 from the mapping.
+	fn encode_evm_address(v: CurrencyId) -> Option<EvmAddress>;
+	/// Decode the CurrencyId from EvmAddress.
+	/// If is CurrencyId::DexShare and contain DexShare::Erc20,
+	/// will use the u32 to get the DexShare::Erc20 from the mapping.
+	fn decode_evm_address(v: EvmAddress) -> Option<CurrencyId>;
+}
